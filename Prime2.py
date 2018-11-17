df0ce330bff26671a205e1f14c715a5a8a1ca830
EktaArora1 <44098671+EktaArora1@users.noreply.github.com>
2018-11-18 02:03:15 +0530

    Update Prime2.py

--- Prime2.py
+++ Prime2.py
@@ -1,8 +1,11 @@
-a=int(input)
-b=int(input)
+def isPrime(j):
+    for i in range(2, int(n**0.5) + 1):
+        if j % i ==0:
+            return False
+    return i
+a=int(input())
+b=int(input())
 for j in range(a,b):
-  for i in range(b):
-    if(j%i==0):
-      break
-    else:
-      print(j)
+  c=isPrime(j)
+  if(c):
+    print(c)
