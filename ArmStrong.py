b2ffec6acbafc73d6a9bc2de29c44417e591d133
EktaArora1 <44098671+EktaArora1@users.noreply.github.com>
2018-11-19 19:54:04 +0530

    Create ArmStrong.py

---
+++ ArmStrong.py
@@ -0,0 +1,20 @@
+a=input()
+b=len(a)
+c=list()
+a=int(a)
+t=a
+for i in range(b):
+    r=t%10
+    t=t//10
+    c.append(r)
+print(c)
+
+s=0
+for k in c:
+    s += k**b
+print(s)
+print(a)
+if(s==a):
+    print('YES')
+else:
+    print('NO')
